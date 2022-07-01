056e93ee8315f75e0ac2862e0bd64d7b4bbaf48e
mach_j <mach_j@etna-alterance.net>
2022-07-01 07:59:57 -0400

    nouveaux tests pour utiliser les boutons

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 var mysql = require('mysql');
 const express = require('express');
 const { response } = require('express');
-const { get } = require('express/lib/response');
 const app = express();
 const port = 5555;
 
@@ -19,27 +18,10 @@ function game(){
     con.query('SELECT name FROM Names ORDER BY RAND() LIMIT 1', function(err, rows) {
     if (err) throw err;
 
-
-        try {
-            var GenderApi = require('gender-api.com-client');
-            var genderApiClient = new GenderApi.Client('xgVbzQndH78QCLGW2ppnTsFqd9rwPBGBuaYt');
-
-            genderApiClient.getByFirstNameAndCountry(rows[0].name, 'FR', function (response) {
-                console.log(response.gender);
-                console.log(response.accuracy);
-                console.log(rows[0].name);
-            });
-
-        }
-        catch(e) {
-            console.log('Error:', e);
-        }
+        checkGender(rows[0].name);
 
         app.set('view engine', 'ejs');
-        app.locals.mascClick = function (){
-            console.log("Vous pensez que ce nom est plutôt masculin");
-            return 1;
-        }
+    
         app.use(express.static("public")); 
         app.get('/', (req, res) => {
             res.render('game', {
@@ -51,12 +33,22 @@ function game(){
         console.log(`App listening at port ${port}`);
         })
 
-        // buttonClicked();
     });
-
-
-    return response;
 }
+
+function checkGender(prenom){
+    var GenderApi = require('gender-api.com-client');
+    var genderApiClient = new GenderApi.Client('xgVbzQndH78QCLGW2ppnTsFqd9rwPBGBuaYt');
+
+    genderApiClient.getByFirstNameAndCountry(prenom, 'FR', function (response) {
+        console.log(response.gender);
+        console.log(response.accuracy);
+        console.log(prenom);
+        console.log(response);
+    });
+}
+
+
 
 // function gameOver(score){
 //     if (score == 20){
@@ -67,14 +59,31 @@ function game(){
 //     }
 // }
 
-// function buttonClicked(){
-//     if (femClick() == 1 || mascClick() == 1){
-//         newScore(score);
-//     }
-//     console.log("prout");
-// }
+function buttonClicked(choosed){
+    if (choosed != ""){
+        newScore(score);
+    }
+    console.log("prout");
+}
 
-
+function newScore(score){
+    // var genderChoosed;
+    // if (femClick() == 1) {
+    //     genderChoosed = "female";
+    // }
+    // if (mascClick() == 1) {
+    //     genderChoosed = "male";
+    // }
+    // if (genderChoosed = response.gender) {
+    //     score += 1;
+    // }
+    // if (genderChoosed != response.gender) {
+    //     score -= 1;
+    // }
+    // gameOver(score);
+    // return score;
+    console.log("pouet");
+}
 
 game();
 
